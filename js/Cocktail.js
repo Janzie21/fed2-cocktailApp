7f33bbcd09ea30ca3a6196496f98171bcfdc8fa8
Janzie21 <nick_janssen21@hotmail.com>
2020-12-14 20:45:25 +0100

    added some styling

--- js/Cocktail.js
+++ js/Cocktail.js
@@ -9,8 +9,12 @@ class Cocktail{
     start(){
         this.holder.insertAdjacentHTML('beforeend', `
         <div class="cocktail">
-
+        <h2>Name: </h2>
+        ${this.cocktailObject.strDrink}
+        <h2>Glass used: </h2>
         ${this.cocktailObject.strGlass}
+        <h2>How to: </h2>
+        <p>${this.cocktailObject.strInstructions}</p>
 
         </div>
 
